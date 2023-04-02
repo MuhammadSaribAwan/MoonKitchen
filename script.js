3f1995aee690b30ebb9af86356ecba9127840030
MuhammadSaribAwan <129158315+MuhammadSaribAwan@users.noreply.github.com>
2023-04-03 00:32:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Funaction Confirm
+let visit = confirm("Are You Realy Want To Our Website");
+if (visit) {
+    console.log("Thank You So Much");
+}
+else {
+    console.log("Thank You");
+}
+// Funaction Prompt
+function name() {
+    console.log(prompt("What Is Your Name?"));
+}
+setTimeout(name, 5000);
+
+// Funaction Prompt
+function Best() {
+    console.log(prompt("Look Is Cool"));
+}
+setTimeout(Best, 20000);
